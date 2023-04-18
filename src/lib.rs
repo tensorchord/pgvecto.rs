9b505fbda99433d793d2995350771729cd141cb8
Keming <kemingyang@tensorchord.ai>
2023-04-18 14:52:49 +0800

    init repo with pgrx
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use pgrx::prelude::*;
+
+pgrx::pg_module_magic!();
+
+#[pg_extern]
+fn hello_vectors() -> &'static str {
+    "Hello, vectors"
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgrx::prelude::*;
+
+    #[pg_test]
+    fn test_hello_vectors() {
+        assert_eq!("Hello, vectors", crate::hello_vectors());
+    }
+
+}
+
+/// This module is required by `cargo pgrx test` invocations.
+/// It must be visible at the root of your extension crate.
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
