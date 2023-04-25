447424dc6ed4fb03a8c9450151e359912621995d
Alex Chi <iskyzh@gmail.com>
2023-04-25 01:44:36 -0400

    feat: implement square_euclidean_distance for f32, f64 and i32 (#12)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/operator.rs
+++ src/operator.rs
@@ -1,9 +1,11 @@
 use pgrx::prelude::*;
 
 /// Square Euclidean distance. Try this one if you don't have any special requirements.
-#[pg_operator(immutable, parallel_safe)]
-#[opname(<->)]
-fn square_euclidean_distance(left: Vec<f32>, right: Vec<f32>) -> f32 {
+fn square_euclidean_distance<T, O>(left: Vec<T>, right: Vec<T>) -> O
+where
+    T: std::ops::Sub<T, Output = T> + Copy,
+    O: std::iter::Sum + std::ops::Mul<O, Output = O> + From<T> + Copy,
+{
     if left.len() != right.len() {
         error!(
             "wrong dimension: left({}) != right({})",
@@ -13,8 +15,30 @@ fn square_euclidean_distance(left: Vec<f32>, right: Vec<f32>) -> f32 {
     }
     left.iter()
         .zip(right.iter())
-        .map(|(x, y)| (x - y).powi(2))
+        .map(|(x, y)| {
+            let z = *x - *y;
+            let z: O = z.into();
+            z * z
+        })
         .sum()
+}
+
+#[pg_operator(immutable, parallel_safe)]
+#[opname(<->)]
+fn square_euclidean_distance_f32(left: Vec<f32>, right: Vec<f32>) -> f32 {
+    square_euclidean_distance::<f32, f32>(left, right)
+}
+
+#[pg_operator(immutable, parallel_safe)]
+#[opname(<->)]
+fn square_euclidean_distance_f64(left: Vec<f64>, right: Vec<f64>) -> f64 {
+    square_euclidean_distance::<f64, f64>(left, right)
+}
+
+#[pg_operator(immutable, parallel_safe)]
+#[opname(<->)]
+fn square_euclidean_distance_i32(left: Vec<i32>, right: Vec<i32>) -> f64 {
+    square_euclidean_distance::<i32, f64>(left, right)
 }
 
 /// Dot product distance.
@@ -56,7 +80,17 @@ mod tests {
 
     #[pg_test]
     fn test_square_euclidean_distance() {
-        let distance = Spi::get_one::<f32>("SELECT ARRAY[0, 1] <-> ARRAY[3, 2]");
+        let distance = Spi::get_one::<f64>("SELECT ARRAY[0, 1]::int[] <-> ARRAY[3, 2]::int[]");
+        assert_eq!(distance, Ok(Some(10.0)));
+        let distance = Spi::get_one::<f32>("SELECT ARRAY[0, 1]::real[] <-> ARRAY[3, 2]::real[]");
+        assert_eq!(distance, Ok(Some(10.0)));
+        let distance = Spi::get_one::<f64>(
+            "SELECT ARRAY[0, 1]::double precision[] <-> ARRAY[3, 2]::double precision[]",
+        );
+        assert_eq!(distance, Ok(Some(10.0)));
+        let distance = Spi::get_one::<f64>("SELECT ARRAY[0, 1] <-> '{3, 2}'");
+        assert_eq!(distance, Ok(Some(10.0)));
+        let distance = Spi::get_one::<f64>("SELECT ARRAY[0, 1] <-> ARRAY[3, 2]");
         assert_eq!(distance, Ok(Some(10.0)));
     }
 
