de6e18316a902dc2db60cd02fe418461a1d5d577
Chengxu Bian <cbian7@wisc.edu>
2023-05-10 22:48:17 -0500

    add unit tests
    Signed-off-by: Chengxu Bian <cbian7@wisc.edu>

--- src/embedding.rs
+++ src/embedding.rs
@@ -6,6 +6,13 @@ use openai_api_rust::{
 pub(crate) type Embedding = Vec<f64>;
 pub(crate) type Embeddings = Vec<Embedding>;
 
+#[cfg(test)]
+use mockall::automock;
+#[cfg_attr(test, automock)]
+pub(crate) trait EmbeddingCreator {
+    fn create_embeddings(&self, input: Vec<String>) -> Result<Embeddings, String>;
+}
+
 const OPENAI_API_URL_BASE: &str = "https://api.openai.com/v1/";
 
 pub(crate) enum EmbeddingModel {
@@ -29,20 +36,26 @@ pub(crate) struct OpenAIEmbedding {
 
 impl OpenAIEmbedding {
     pub(crate) fn new_ada002(api_key: String) -> Self {
-        Self::new(api_key, EmbeddingModel::Ada002)
+        Self::new(
+            api_key,
+            EmbeddingModel::Ada002,
+            OPENAI_API_URL_BASE.to_string(),
+        )
     }
 
-    pub(crate) fn new(api_key: String, model: EmbeddingModel) -> Self {
+    pub(crate) fn new(api_key: String, model: EmbeddingModel, api_url: String) -> Self {
         Self {
             api_key,
             model,
-            api_url: OPENAI_API_URL_BASE.to_string(),
+            api_url,
         }
     }
+}
 
-    pub(crate) fn create_embeddings(&self, input: Vec<String>) -> Result<Embeddings, String> {
+impl EmbeddingCreator for OpenAIEmbedding {
+    fn create_embeddings(&self, input: Vec<String>) -> Result<Embeddings, String> {
         let auth = Auth::new(&self.api_key);
-        let client = OpenAI::new(auth, &self.api_url);
+        let client = OpenAI::new(auth, &self.api_url).use_env_proxy();
 
         let embeddings = client
             .embeddings_create(&EmbeddingsBody {
@@ -60,5 +73,56 @@ impl OpenAIEmbedding {
             .collect();
 
         Ok(data)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use httpmock::MockServer;
+    use serde_json::json;
+
+    use crate::embedding::{Embedding, EmbeddingCreator};
+
+    use super::OpenAIEmbedding;
+
+    #[test]
+    fn test_create_embeddings() {
+        let input: String = "hello".to_string();
+        let expected_emb: Embedding = vec![0.151240, 0.1231224123, 0.231253124, 0.213125];
+        let server = MockServer::start();
+
+        let emb_mock = server.mock(|when, then| {
+            when.path("/embeddings");
+            then.json_body(format_output_json(expected_emb.clone()));
+        });
+
+        let client = OpenAIEmbedding::new(
+            "".to_string(),
+            crate::embedding::EmbeddingModel::Ada002,
+            server.base_url() + "/",
+        );
+
+        let mut embs = client.create_embeddings(vec![input]).unwrap();
+
+        let emb = embs.pop().unwrap();
+        assert_eq!(emb, expected_emb);
+        emb_mock.assert();
+    }
+
+    fn format_output_json(embedding: Embedding) -> serde_json::Value {
+        json!({
+          "data": [
+            {
+              "embedding": embedding,
+              "index": 0,
+              "object": "embedding"
+            }
+          ],
+          "model": "text-embedding-ada-002",
+          "object": "list",
+          "usage": {
+            "prompt_tokens": 0,
+            "total_tokens": 0
+          }
+        })
     }
 }
--- src/udf.rs
+++ src/udf.rs
@@ -1,11 +1,11 @@
 use pgrx::prelude::*;
 
 use crate::{
-    embedding::{Embedding, OpenAIEmbedding},
+    embedding::{Embedding, EmbeddingCreator, OpenAIEmbedding},
     gucs::OPENAI_API_KEY_GUC,
 };
 
-#[pg_extern(immutable)]
+#[pg_extern]
 fn ai_embedding_vector(input: String) -> Embedding {
     let api_key = match OPENAI_API_KEY_GUC.get() {
         Some(key) => key,
@@ -17,25 +17,76 @@ fn ai_embedding_vector(input: String) -> Embedding {
     // default to use ada002
     let openai_embedding = OpenAIEmbedding::new_ada002(api_key);
 
-    match openai_embedding.create_embeddings(vec![input]) {
-        Ok(mut embeddings) => match embeddings.pop() {
-            Some(embedding) => embedding,
-            None => {
-                error!("embedding is empty")
-            }
-        },
+    match ai_embedding_vector_inner(input, openai_embedding) {
+        Ok(embedding) => embedding,
         Err(e) => {
-            error!("failed to create embedding, {}", e)
+            error!("{}", e)
         }
     }
 }
 
-#[cfg(any(test, feature = "pg_test"))]
-#[pg_schema]
-mod tests {
-    use pgrx::prelude::*;
+fn ai_embedding_vector_inner(
+    input: String,
+    client: impl EmbeddingCreator,
+) -> Result<Embedding, String> {
+    match client.create_embeddings(vec![input]) {
+        Ok(mut embeddings) => match embeddings.pop() {
+            Some(embedding) => Ok(embedding),
+            None => Err("embedding is empty".to_string()),
+        },
+        Err(e) => Err(format!("failed to create embedding, {}", e)),
+    }
+}
 
-    // We need to mock embedding requests since it requires an API key.
-    #[pg_test]
-    fn test_ai_embedding_vector() {}
+#[cfg(test)]
+mod tests {
+    use crate::embedding::MockEmbeddingCreator;
+    use crate::udf::ai_embedding_vector_inner;
+    use mockall::predicate::eq;
+
+    // We need to mock embedding since it requires an API key.
+    #[test]
+    fn test_ai_embedding_vector_inner_successful() {
+        let input = String::from("input");
+        let mut mock_client = MockEmbeddingCreator::new();
+        let expected_embedding = vec![1.0, 2.0, 3.0];
+        mock_client
+            .expect_create_embeddings()
+            .with(eq(vec![input.clone()]))
+            .returning(|_| Ok(vec![vec![1.0, 2.0, 3.0]]));
+
+        let result = ai_embedding_vector_inner(input, mock_client);
+
+        assert_eq!(result, Ok(expected_embedding));
+    }
+
+    #[test]
+    fn test_ai_embedding_vector_inner_empty_embedding() {
+        let input = String::from("input");
+        let mut mock_client = MockEmbeddingCreator::new();
+        mock_client
+            .expect_create_embeddings()
+            .with(eq(vec![input.clone()]))
+            .returning(|_| Ok(vec![]));
+
+        let result = ai_embedding_vector_inner(input, mock_client);
+        assert_eq!(result, Err("embedding is empty".to_string()))
+    }
+
+    #[test]
+    fn test_ai_embedding_vector_inner_error() {
+        let input = String::from("input");
+        let mut mock_client = MockEmbeddingCreator::new();
+        mock_client
+            .expect_create_embeddings()
+            .with(eq(vec![input.clone()]))
+            .returning(|_| Err(String::from("invalid input")));
+
+        let result = ai_embedding_vector_inner(input, mock_client);
+
+        assert_eq!(
+            result,
+            Err("failed to create embedding, invalid input".to_string())
+        )
+    }
 }
