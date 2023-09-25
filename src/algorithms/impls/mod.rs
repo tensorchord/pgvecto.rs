7dad039ecdf7754cce5dacb6ec69a9ac64df1dab
Avery <averyqi115@gmail.com>
2023-09-25 13:07:40 -0400

    feat: vamana (#64)
    * add vamana
    
    Signed-off-by: Avery Qi <2225992901@qq.com>
    
    * fix
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * fix unused variable; dead code and incompatible data types
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * fix
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * WIP load
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * finish concurrent without par_iter
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * format
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * use n instead of r for small dataset
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * apply filter
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * rebase on recent PR
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * update readme for vamana usage
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    * allow unused assignments
    
    Signed-off-by: Avery115 <2225992901@qq.com>
    
    ---------
    
    Signed-off-by: Avery Qi <2225992901@qq.com>
    Signed-off-by: Avery115 <2225992901@qq.com>

--- src/algorithms/impls/mod.rs
+++ src/algorithms/impls/mod.rs
@@ -2,3 +2,4 @@ pub mod elkan_k_means;
 pub mod hnsw;
 pub mod ivf;
 pub mod quantization;
+pub mod vamana;
---
+++ src/algorithms/vamana.rs
@@ -0,0 +1,129 @@
+use super::impls::vamana::VamanaImpl;
+use super::Algo;
+use crate::bgworker::index::IndexOptions;
+use crate::bgworker::storage::Storage;
+use crate::bgworker::storage::StoragePreallocator;
+use crate::bgworker::vectors::Vectors;
+use crate::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use thiserror::Error;
+
+#[derive(Debug, Clone, Error, Serialize, Deserialize)]
+pub enum VamanaError {
+    //
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct VamanaOptions {
+    #[serde(default = "VamanaOptions::default_memmap")]
+    pub memmap: Memmap,
+    /// out degree bound
+    #[serde(default = "VamanaOptions::default_r")]
+    pub r: usize,
+    /// Distance threshold
+    #[serde(default = "VamanaOptions::default_alpha")]
+    pub alpha: f32,
+    /// Search list size
+    #[serde(default = "VamanaOptions::default_l")]
+    pub l: usize,
+    #[serde(default = "VamanaOptions::default_build_threads")]
+    pub build_threads: usize,
+}
+
+impl VamanaOptions {
+    fn default_memmap() -> Memmap {
+        Memmap::Ram
+    }
+    fn default_r() -> usize {
+        50
+    }
+    fn default_alpha() -> f32 {
+        1.2
+    }
+    fn default_l() -> usize {
+        70
+    }
+    fn default_build_threads() -> usize {
+        std::thread::available_parallelism().unwrap().get()
+    }
+}
+
+pub struct Vamana<D: DistanceFamily> {
+    implementation: VamanaImpl<D>,
+}
+
+impl<D: DistanceFamily> Algo for Vamana<D> {
+    type Error = VamanaError;
+
+    fn prebuild(
+        storage: &mut StoragePreallocator,
+        options: IndexOptions,
+    ) -> Result<(), Self::Error> {
+        let vamana_options = options.algorithm.clone().unwrap_vamana();
+        VamanaImpl::<D>::prebuild(
+            storage,
+            options.capacity,
+            vamana_options.r,
+            vamana_options.memmap,
+        )?;
+        Ok(())
+    }
+
+    fn build(
+        storage: &mut Storage,
+        options: IndexOptions,
+        vectors: Arc<Vectors>,
+        n: usize,
+    ) -> Result<Self, VamanaError> {
+        let vamana_options = options.algorithm.clone().unwrap_vamana();
+        let implementation = VamanaImpl::new(
+            storage,
+            vectors,
+            n,
+            options.capacity,
+            options.dims,
+            vamana_options.r,
+            vamana_options.alpha,
+            vamana_options.l,
+            vamana_options.build_threads,
+            vamana_options.memmap,
+        )?;
+        Ok(Self { implementation })
+    }
+
+    fn load(
+        storage: &mut Storage,
+        options: IndexOptions,
+        vectors: Arc<Vectors>,
+    ) -> Result<Self, VamanaError> {
+        let vamana_options = options.algorithm.unwrap_vamana();
+        let implementation = VamanaImpl::load(
+            storage,
+            vectors,
+            options.capacity,
+            options.dims,
+            vamana_options.r,
+            vamana_options.alpha,
+            vamana_options.l,
+            vamana_options.build_threads,
+            vamana_options.memmap,
+        )?;
+        Ok(Self { implementation })
+    }
+    #[allow(unused)]
+    fn insert(&self, insert: usize) -> Result<(), VamanaError> {
+        Ok(self.implementation.insert(insert)?)
+    }
+    fn search<F>(
+        &self,
+        target: Box<[Scalar]>,
+        k: usize,
+        filter: F,
+    ) -> Result<Vec<(Scalar, u64)>, VamanaError>
+    where
+        F: FnMut(u64) -> bool,
+    {
+        Ok(self.implementation.search(target, k, filter)?)
+    }
+}
