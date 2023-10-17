244f431737c6e526284feb86d48ee3d74ec2c9b4
BeautyyuYanli <beautyyuyanli@gmail.com>
2023-10-17 15:18:49 +0800

    chore: change requirement.txt
    Signed-off-by: 盐粒 Yanli <mail@yanli.one>

--- bindings/python/tests/__init__.py
+++ bindings/python/tests/__init__.py
@@ -1,14 +1,14 @@
 import os
 
-PORT = os.getenv('DB_PORT', 5432)
-HOST = os.getenv('DB_HOST', 'localhost')
-USER = os.getenv('DB_USER', 'postgres')
-PASS = os.getenv('DB_PASS', 'password')
-DB_NAME = os.getenv('DB_NAME', 'postgres')
+PORT = os.getenv("DB_PORT", 5432)
+HOST = os.getenv("DB_HOST", "localhost")
+USER = os.getenv("DB_USER", "postgres")
+PASS = os.getenv("DB_PASS", "mysecretpassword")
+DB_NAME = os.getenv("DB_NAME", "postgres")
 
 # Run tests with shell:
 #   DB_HOST=localhost DB_USER=postgres DB_PASS=password DB_NAME=postgres python3 -m pytest bindings/python/tests/
-URL = 'postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}'.format(
+URL = "postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}".format(
     port=PORT,
     host=HOST,
     username=USER,
