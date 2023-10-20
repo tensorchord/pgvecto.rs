cd5ed956786b36d6dc6968c534eb0d94be121133
盐粒 Yanli <mail@yanli.one>
2023-10-20 15:24:34 +0800

    feat: delete serializer for binary (since it's not available for now)
    Signed-off-by: 盐粒 Yanli <mail@yanli.one>

--- bindings/python/pgvector_rs/utils/serializer.py
+++ bindings/python/pgvector_rs/utils/serializer.py
@@ -11,29 +11,10 @@ def from_db_str(value):
 
 
 @decorators.ignore_none
-@decorators.ignore_ndarray  # could be ndarray if already cast by lower-level driver
-def from_db_binary(value):
-    (dim, unused) = unpack(">HH", value[:4])
-    return np.frombuffer(value, dtype=">f", count=dim, offset=4).astype(
-        dtype=np.float32
-    )
-
-
-@decorators.ignore_none
 @decorators.validate_builtin_list
 @decorators.validate_ndarray
-def to_db_str(value: np.ndarray, dim: Optional[int]=None):
+def to_db_str(value: np.ndarray, dim: Optional[int] = None):
     if dim is not None and value.shape[0] != dim:
         raise ValueError("expected %d dimensions, not %d" % (dim, len(value)))
 
     return "[" + ",".join([str(float(v)) for v in value]) + "]"
-
-
-@decorators.ignore_none
-@decorators.validate_builtin_list
-@decorators.validate_ndarray
-def to_db_binary(value: np.ndarray, dim=None):
-    if dim is not None and value.shape[0] != dim:
-        raise ValueError("expected %d dimensions, not %d" % (dim, len(value)))
-
-    return pack(">HH", value.shape[0], 0) + value.astype(">f").tobytes()
