fb017e2f8b82049537f41d71d99df01f2067eb87
usamoi <usamoi@outlook.com>
2023-11-09 18:08:55 +0800

    fix: mark cast functions immutable
    Signed-off-by: usamoi <usamoi@outlook.com>

--- src/postgres/casts.rs
+++ src/postgres/casts.rs
@@ -1,7 +1,7 @@
 use super::datatype::{Vector, VectorInput, VectorOutput, VectorTypmod};
 use crate::prelude::Scalar;
 
-#[pgrx::pg_extern]
+#[pgrx::pg_extern(immutable, parallel_safe, strict)]
 fn cast_array_to_vector(array: pgrx::Array<Scalar>, typmod: i32, _explicit: bool) -> VectorOutput {
     assert!(!array.is_empty());
     assert!(!array.contains_nulls());
@@ -13,7 +13,7 @@ fn cast_array_to_vector(array: pgrx::Array<Scalar>, typmod: i32, _explicit: bool
     Vector::new_in_postgres(&data)
 }
 
-#[pgrx::pg_extern]
+#[pgrx::pg_extern(immutable, parallel_safe, strict)]
 fn cast_vector_to_array<'a>(vector: VectorInput<'a>, _typmod: i32, _explicit: bool) -> Vec<Scalar> {
     vector.data().to_vec()
 }
