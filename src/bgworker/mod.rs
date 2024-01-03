7ecc36212ff395d79477feff83d64a5999d67453
usamoi <usamoi@outlook.com>
2024-01-03 19:52:24 +0800

    chore: friendly OOM message
    Signed-off-by: usamoi <usamoi@outlook.com>

--- src/bgworker/mod.rs
+++ src/bgworker/mod.rs
@@ -4,12 +4,14 @@ pub mod upgrade;
 pub unsafe fn init() {
     use pgrx::bgworkers::BackgroundWorkerBuilder;
     use pgrx::bgworkers::BgWorkerStartTime;
+    use std::time::Duration;
     BackgroundWorkerBuilder::new("vectors")
         .set_function("vectors_main")
         .set_library("vectors")
         .set_argument(None)
         .enable_shmem_access(None)
         .set_start_time(BgWorkerStartTime::PostmasterStart)
+        .set_restart_time(Some(Duration::from_secs(1)))
         .load();
 }
 
@@ -19,6 +21,9 @@ extern "C" fn vectors_main(_arg: pgrx::pg_sys::Datum) {
 }
 
 pub fn main() {
+    pub struct AllocErrorPanicPayload {
+        pub layout: std::alloc::Layout,
+    }
     {
         let mut builder = env_logger::builder();
         builder.target(env_logger::Target::Stderr);
@@ -33,6 +38,10 @@ pub fn main() {
         builder.init();
     }
     std::panic::set_hook(Box::new(|info| {
+        if let Some(oom) = info.payload().downcast_ref::<AllocErrorPanicPayload>() {
+            log::error!("Out of memory. Layout: {:?}.", oom.layout);
+            return;
+        }
         let backtrace;
         #[cfg(not(debug_assertions))]
         {
@@ -44,6 +53,9 @@ pub fn main() {
         }
         log::error!("Panickied. Info: {:?}. Backtrace: {}.", info, backtrace);
     }));
+    std::alloc::set_alloc_error_hook(|layout| {
+        std::panic::panic_any(AllocErrorPanicPayload { layout });
+    });
     use service::worker::Worker;
     use std::path::Path;
     let path = Path::new("pg_vectors");
