e1fde8624994d925b2f5147aebc36518bc26c73e
Joe Passanante <28711605+JoePassanante@users.noreply.github.com>
2024-01-13 20:26:59 -0800

    feat: Add vector to vecf16 CAST (#266)
    Signed-off-by: jepassanante <j.e.passanante@gmail.com>

--- src/datatype/casts_f32.rs
+++ src/datatype/casts_f32.rs
@@ -1,5 +1,8 @@
 use crate::datatype::typmod::Typmod;
+use crate::datatype::vecf16::{Vecf16, Vecf16Output};
 use crate::datatype::vecf32::{Vecf32, Vecf32Input, Vecf32Output};
+
+use half::f16;
 use service::prelude::*;
 
 #[pgrx::pg_extern(immutable, parallel_safe, strict)]
@@ -27,4 +30,21 @@ fn _vectors_cast_vecf32_to_array(
     _explicit: bool,
 ) -> Vec<f32> {
     vector.data().iter().map(|x| x.to_f32()).collect()
+}
+
+#[pgrx::pg_extern(immutable, parallel_safe, strict)]
+fn _vectors_cast_vecf32_to_vecf16(
+    vector: Vecf32Input<'_>,
+    _typmod: i32,
+    _explicit: bool,
+) -> Vecf16Output {
+    let data: Vec<F16> = vector
+        .data()
+        .iter()
+        .map(|x| x.to_f32())
+        .map(f16::from_f32)
+        .map(F16::from)
+        .collect();
+
+    Vecf16::new_in_postgres(&data)
 }
