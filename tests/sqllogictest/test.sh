43fdc190587d13ace76e8355ae086a369df1930d
usamoi <usamoi@outlook.com>
2024-02-02 16:41:59 +0800

    test: pg14 tests
    Signed-off-by: usamoi <usamoi@outlook.com>

--- tests/sqllogictest/test.sh
+++ tests/sqllogictest/test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-sqllogictest -u runner -d runner $(dirname $0)/*.slt
+sqllogictest -d $USER $(dirname $0)/*.slt
+
+if [ "$(psql -tAqX -c "SHOW server_version_num")" -ge 140000 ]; then
+    sqllogictest -d $USER $(dirname $0)/pg14/*.slt
+fi
