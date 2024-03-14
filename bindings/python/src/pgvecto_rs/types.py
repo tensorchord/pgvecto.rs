f08f9b44328c5f40fbc89b10505f8ebc839e9491
my-vegetable-has-exploded <wy1109468038@gmail.com>
2024-03-14 14:02:35 +0000

    fix typos.
    Signed-off-by: my-vegetable-has-exploded <wy1109468038@gmail.com>

--- bindings/python/src/pgvecto_rs/types.py
+++ bindings/python/src/pgvecto_rs/types.py
@@ -1,11 +1,11 @@
 import collections
 
-SparseVector = collections.namedtuple("SparseVector", "dims indexs values")
+SparseVector = collections.namedtuple("SparseVector", "dims indices values")
 
 
 def print_sparse_vector(sparse_vector):
     dense = [0.0] * sparse_vector.dims
-    for i, v in zip(sparse_vector.indexs, sparse_vector.values):
+    for i, v in zip(sparse_vector.indices, sparse_vector.values):
         dense[i] = v
     return str(dense)
 
