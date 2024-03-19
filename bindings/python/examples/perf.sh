2821fafc854abcfb279e1a48573ab3dd5d0ebaeb
my-vegetable-has-exploded <wy1109468038@gmail.com>
2024-03-19 04:54:57 +0000

    rm perf.sh & refactor examples.
    Signed-off-by: my-vegetable-has-exploded <wy1109468038@gmail.com>

--- bindings/python/examples/perf.sh
+++
@@ -1,5 +0,0 @@
-/bin/python3 /home/envd/pgvecto.rs/bindings/python/examples/psycopg_testcpy.py &
-PID=$(ps aux | grep "/usr/lib/postgresql/15/bin/postgres" | grep -v grep | awk '{print $2}')
-# use perf to collect data
-sudo /usr/lib/linux-tools/5.15.0-97-generic/perf record -e cpu-clock -F 1000 -g -p $PID -- sleep 30
-sudo /usr/lib/linux-tools/5.15.0-97-generic/perf script -i perf.data > insert.out.perf
