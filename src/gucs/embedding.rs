d8cce9218cae4a9f93e45e12dd617a8255628e59
yi wang <48236141+my-vegetable-has-exploded@users.noreply.github.com>
2024-04-20 09:51:19 +0800

    fix openai option. (#469)
    Signed-off-by: my-vegetable-has-exploded <wy1109468038@gmail.com>
    Signed-off-by: jinweios <jinwei.peng@beingthink.com>

--- src/gucs/embedding.rs
+++ src/gucs/embedding.rs
@@ -4,7 +4,7 @@ use pgrx::{GucContext, GucFlags, GucRegistry, GucSetting};
 use std::ffi::CStr;
 
 pub fn openai_options() -> OpenAIOptions {
-    let base_url = guc_string_parse(&OPENAI_BASE_URL, "vectors.openai_base");
+    let base_url = guc_string_parse(&OPENAI_BASE_URL, "vectors.openai_base_url");
     let api_key = guc_string_parse(&OPENAI_API_KEY, "vectors.openai_api_key");
     OpenAIOptions { base_url, api_key }
 }
@@ -13,7 +13,7 @@ static OPENAI_API_KEY: GucSetting<Option<&'static CStr>> =
     GucSetting::<Option<&'static CStr>>::new(None);
 
 static OPENAI_BASE_URL: GucSetting<Option<&'static CStr>> =
-    GucSetting::<Option<&'static CStr>>::new(Some(c"https://api.openai.com/v1/"));
+    GucSetting::<Option<&'static CStr>>::new(Some(c"https://api.openai.com/v1"));
 
 pub unsafe fn init() {
     GucRegistry::define_string_guc(
