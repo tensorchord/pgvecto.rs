430733d318c2c917492667cb087d18ff504c15dd
usamoi <usamoi@outlook.com>
2024-06-05 09:48:01 +0800

    ci: fix update_vendor (#496)
    * ci: fix update_vendor
    
    Signed-off-by: usamoi <usamoi@outlook.com>
    
    * chore: update vendor
    
    ---------
    
    Signed-off-by: usamoi <usamoi@outlook.com>

--- scripts/update_vendor.sh
+++ scripts/update_vendor.sh
@@ -2,10 +2,10 @@
 set -e
 
 printf "VERSION = ${VERSION}\n"
-printf "PGRX = ${PGRX}\n"
+printf "BRANCH = ${BRANCH}\n"
 
 apt-get update
-apt-get install -y --no-install-recommends ca-certificates curl build-essential gnupg lsb-release wget
+apt-get install -y --no-install-recommends ca-certificates curl build-essential gnupg lsb-release wget git
 
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | tee -a /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
@@ -18,7 +18,7 @@ source ~/.cargo/env
 cd $(mktemp -d)
 
 cargo init --lib --name vectors
-cargo add pgrx-pg-sys@=$PGRX --no-default-features --features pg$VERSION
+cargo add pgrx-pg-sys --git https://github.com/tensorchord/pgrx.git --branch $BRANCH --no-default-features --features pg$VERSION
 PGRX_PG_CONFIG_PATH=$(which pg_config) PGRX_PG_SYS_EXTRA_OUTPUT_PATH=$(pwd)/pgrx-binding.rs cargo build
 rustfmt ./pgrx-binding.rs
 
