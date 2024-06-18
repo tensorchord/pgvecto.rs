8a8561e8765c2c677caa2cde0cdb8bf080c11b0b
cutecutecat <junyuchen@tensorchord.ai>
2024-06-18 10:22:11 +0800

    fix: by comments
    Signed-off-by: cutecutecat <junyuchen@tensorchord.ai>

--- src/datatype/text_svecf32.rs
+++ src/datatype/text_svecf32.rs
@@ -5,10 +5,11 @@ use base::scalar::*;
 use base::vector::*;
 use pgrx::pg_sys::Oid;
 use std::ffi::{CStr, CString};
+use std::fmt::Write;
 
 #[pgrx::pg_extern(immutable, strict, parallel_safe)]
 fn _vectors_svecf32_in(input: &CStr, _oid: Oid, _typmod: i32) -> SVecf32Output {
-    use crate::utils::parse::parse_pgvector_svector;
+    use crate::utils::parse::{parse_pgvector_svector, svector_filter_nonzero};
     let v = parse_pgvector_svector(input.to_bytes(), |s| s.parse::<F32>().ok());
     match v {
         Err(e) => {
@@ -16,7 +17,13 @@ fn _vectors_svecf32_in(input: &CStr, _oid: Oid, _typmod: i32) -> SVecf32Output {
         }
         Ok((indexes, values, dims)) => {
             check_value_dims_1048575(dims);
-            SVecf32Output::new(SVecf32Borrowed::new(dims as u32, &indexes, &values))
+            check_index_in_bound(&indexes, dims);
+            let (non_zero_indexes, non_zero_values) = svector_filter_nonzero(&indexes, &values);
+            SVecf32Output::new(SVecf32Borrowed::new(
+                dims as u32,
+                &non_zero_indexes,
+                &non_zero_values,
+            ))
         }
     }
 }
@@ -27,16 +34,16 @@ fn _vectors_svecf32_out(vector: SVecf32Input<'_>) -> CString {
     let mut buffer = String::new();
     buffer.push('{');
     let svec = vector.for_borrow();
-    let mut need_splitter = true;
+    let mut need_splitter = false;
     for (&index, &value) in svec.indexes().iter().zip(svec.values().iter()) {
         match need_splitter {
-            true => {
-                buffer.push_str(format!("{}:{}", index, value).as_str());
-                need_splitter = false;
+            false => {
+                write!(buffer, "{}:{}", index, value).unwrap();
+                need_splitter = true;
             }
-            false => buffer.push_str(format!(", {}:{}", index, value).as_str()),
+            true => write!(buffer, ", {}:{}", index, value).unwrap(),
         }
     }
-    buffer.push_str(format!("}}/{}", dims).as_str());
+    write!(buffer, "}}/{}", dims).unwrap();
     CString::new(buffer).unwrap()
 }
--- src/utils/parse.rs
+++ src/utils/parse.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use num_traits::Zero;
 use thiserror::Error;
 
@@ -17,12 +15,6 @@ pub enum ParseVectorError {
     TooShortNumber { position: usize },
     #[error("Bad parsing at position {position}")]
     BadParsing { position: usize },
-    #[error("Index out of bounds: the dim is {dims} but the index is {index}")]
-    OutOfBound { dims: usize, index: usize },
-    #[error("The dimension should be {min} < dim < {max}, but it is actually {dims}")]
-    InvalidDimension { dims: usize, min: usize, max: usize },
-    #[error("Indexes need to be unique, but there are more than one same index {index}")]
-    IndexConflict { index: usize },
 }
 
 #[inline(always)]
@@ -93,7 +85,7 @@ where
     Ok(vector)
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum ParseState {
     Start,
     LeftBracket,
@@ -102,6 +94,26 @@ enum ParseState {
     Splitter,
     Comma,
     Length,
+}
+
+#[inline(always)]
+pub fn svector_filter_nonzero<T: Zero + Clone + PartialEq>(
+    indexes: &[u32],
+    values: &[T],
+) -> (Vec<u32>, Vec<T>) {
+    let non_zero_indexes: Vec<u32> = indexes
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| values.get(*i).unwrap() != &T::zero())
+        .map(|(_, x)| *x)
+        .collect();
+    let non_zero_values: Vec<T> = indexes
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| values.get(*i).unwrap() != &T::zero())
+        .map(|(i, _)| values.get(i).unwrap().clone())
+        .collect();
+    (non_zero_indexes, non_zero_values)
 }
 
 #[inline(always)]
@@ -117,155 +129,87 @@ where
         return Err(ParseVectorError::EmptyString {});
     }
     let mut token: ArrayVec<u8, 48> = ArrayVec::new();
-
     let mut indexes = Vec::<u32>::new();
     let mut values = Vec::<T>::new();
-    let mut all_indexes = Vec::<u32>::new();
-    let mut index: u32 = u32::MAX;
 
     let mut state = ParseState::Start;
-    let mut position = 0;
-    loop {
-        if position >= input.len() {
-            break;
-        }
-        match state {
-            ParseState::Start => {
-                let c = input[position];
-                match c {
-                    b'{' => {
-                        state = ParseState::LeftBracket;
-                    }
-                    b' ' => {}
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
+    for (position, char) in input.iter().enumerate() {
+        let c = *char;
+        match (&state, c) {
+            (_, b' ') => {}
+            (ParseState::Start, b'{') => {
+                state = ParseState::LeftBracket;
+            }
+            (
+                ParseState::LeftBracket | ParseState::Index | ParseState::Comma,
+                b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'.' | b'+' | b'-',
+            ) => {
+                if token.is_empty() {
+                    token.push(b'$');
+                }
+                if token.try_push(c).is_err() {
+                    return Err(ParseVectorError::TooLongNumber { position });
+                }
+                state = ParseState::Index;
+            }
+            (ParseState::LeftBracket | ParseState::Comma, b'}') => {
+                state = ParseState::Splitter;
+            }
+            (ParseState::Index, b':') => {
+                if token.is_empty() {
+                    return Err(ParseVectorError::TooShortNumber { position });
+                }
+                let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
+                let index = s
+                    .parse::<u32>()
+                    .map_err(|_| ParseVectorError::BadParsing { position })?;
+                indexes.push(index);
+                token.clear();
+                state = ParseState::Value;
+            }
+            (ParseState::Value, b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'.' | b'+' | b'-') => {
+                if token.is_empty() {
+                    token.push(b'$');
+                }
+                if token.try_push(c).is_err() {
+                    return Err(ParseVectorError::TooLongNumber { position });
                 }
             }
-            ParseState::LeftBracket => {
-                let c = input[position];
-                match c {
-                    b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'.' | b'+' | b'-' => {
-                        // Do not read it here, goto Index to read
-                        position -= 1;
-                        state = ParseState::Index;
-                    }
-                    b'}' => {
-                        state = ParseState::Splitter;
-                    }
-                    b' ' => {}
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
+            (ParseState::Value, b',') => {
+                if token.is_empty() {
+                    return Err(ParseVectorError::TooShortNumber { position });
+                }
+                let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
+                let num = f(s).ok_or(ParseVectorError::BadParsing { position })?;
+                values.push(num);
+                token.clear();
+                state = ParseState::Comma;
+            }
+            (ParseState::Value, b'}') => {
+                if token.is_empty() {
+                    return Err(ParseVectorError::TooShortNumber { position });
+                }
+                let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
+                let num = f(s).ok_or(ParseVectorError::BadParsing { position })?;
+                values.push(num);
+                token.clear();
+                state = ParseState::Splitter;
+            }
+            (ParseState::Splitter, b'/') => {
+                state = ParseState::Length;
+            }
+            (ParseState::Length, b'0'..=b'9') => {
+                if token.is_empty() {
+                    token.push(b'$');
+                }
+                if token.try_push(c).is_err() {
+                    return Err(ParseVectorError::TooLongNumber { position });
                 }
             }
-            ParseState::Index => {
-                let c = input[position];
-                match c {
-                    b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'.' | b'+' | b'-' => {
-                        if token.is_empty() {
-                            token.push(b'$');
-                        }
-                        if token.try_push(c).is_err() {
-                            return Err(ParseVectorError::TooLongNumber { position });
-                        }
-                    }
-                    b':' => {
-                        if token.is_empty() {
-                            return Err(ParseVectorError::TooShortNumber { position });
-                        }
-                        let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
-                        index = s
-                            .parse::<u32>()
-                            .map_err(|_| ParseVectorError::BadParsing { position })?;
-                        token.clear();
-                        state = ParseState::Value;
-                    }
-                    b' ' => {}
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
-                }
-            }
-            ParseState::Value => {
-                let c = input[position];
-                match c {
-                    b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'.' | b'+' | b'-' => {
-                        if token.is_empty() {
-                            token.push(b'$');
-                        }
-                        if token.try_push(c).is_err() {
-                            return Err(ParseVectorError::TooLongNumber { position });
-                        }
-                    }
-                    b',' => {
-                        if token.is_empty() {
-                            return Err(ParseVectorError::TooShortNumber { position });
-                        }
-                        let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
-                        let num = f(s).ok_or(ParseVectorError::BadParsing { position })?;
-                        if !num.is_zero() {
-                            indexes.push(index);
-                            values.push(num);
-                        }
-                        all_indexes.push(index);
-                        token.clear();
-                        state = ParseState::Comma;
-                    }
-                    // Bracket ended with number
-                    b'}' => {
-                        if token.is_empty() {
-                            return Err(ParseVectorError::TooShortNumber { position });
-                        }
-                        let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
-                        let num = f(s).ok_or(ParseVectorError::BadParsing { position })?;
-                        if !num.is_zero() {
-                            indexes.push(index);
-                            values.push(num);
-                        }
-                        all_indexes.push(index);
-                        token.clear();
-                        state = ParseState::Splitter;
-                    }
-                    b' ' => {}
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
-                }
-            }
-            ParseState::Comma => {
-                let c = input[position];
-                match c {
-                    b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'.' | b'+' | b'-' => {
-                        // Do not read it here, goto Index to read
-                        position -= 1;
-                        state = ParseState::Index;
-                    }
-                    b'}' => {
-                        // Bracket ended with comma
-                        state = ParseState::Splitter;
-                    }
-                    b' ' => {}
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
-                }
-            }
-            ParseState::Splitter => {
-                let c = input[position];
-                match c {
-                    b'/' => {
-                        state = ParseState::Length;
-                    }
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
-                }
-            }
-            ParseState::Length => {
-                let c = input[position];
-                match c {
-                    b'0'..=b'9' => {
-                        if token.is_empty() {
-                            token.push(b'$');
-                        }
-                        if token.try_push(c).is_err() {
-                            return Err(ParseVectorError::TooLongNumber { position });
-                        }
-                    }
-                    _ => return Err(ParseVectorError::BadCharacter { position }),
-                }
+            (_, _) => {
+                return Err(ParseVectorError::BadCharacter { position });
             }
         }
-        position += 1;
     }
     if state != ParseState::Length {
         return Err(ParseVectorError::BadParsing {
@@ -273,42 +217,16 @@ where
         });
     }
     if token.is_empty() {
-        return Err(ParseVectorError::TooShortNumber { position });
+        return Err(ParseVectorError::TooShortNumber {
+            position: input.len(),
+        });
     }
     let s = unsafe { std::str::from_utf8_unchecked(&token[1..]) };
     let dims = s
         .parse::<usize>()
-        .map_err(|_| ParseVectorError::BadParsing { position })?;
-
-    // Check dimension out of bound
-    if dims == 0 || dims >= 1048576 {
-        return Err(ParseVectorError::InvalidDimension {
-            dims,
-            min: 0,
-            max: 1048576,
-        });
-    }
-    // Check index out of bound
-    for index in all_indexes.clone() {
-        if index as usize >= dims {
-            return Err(ParseVectorError::OutOfBound {
-                dims,
-                index: index as usize,
-            });
-        }
-    }
-    // Check index conflicts
-    let mut result: HashMap<u32, usize> = HashMap::new();
-    for index in all_indexes {
-        if let Some(value) = result.get(&index) {
-            if *value == 1 {
-                return Err(ParseVectorError::IndexConflict {
-                    index: index as usize,
-                });
-            }
-        }
-        *result.entry(index).or_insert(0) += 1;
-    }
+        .map_err(|_| ParseVectorError::BadParsing {
+            position: input.len(),
+        })?;
 
     let mut indices = (0..indexes.len()).collect::<Vec<_>>();
     indices.sort_by_key(|&i| &indexes[i]);
@@ -317,20 +235,19 @@ where
         .map(|i| values.get(*i).unwrap().clone())
         .collect();
     indexes.sort();
+
     Ok((indexes, sorted_values, dims))
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
     use base::scalar::F32;
 
     use super::*;
 
     #[test]
     fn test_svector_parse_accept() {
-        let exprs: HashMap<&str, (Vec<u32>, Vec<F32>, usize)> = HashMap::from([
+        let exprs: Vec<(&str, (Vec<u32>, Vec<F32>, usize))> = vec![
             ("{}/1", (vec![], vec![], 1)),
             ("{0:1}/1", (vec![0], vec![F32(1.0)], 1)),
             (
@@ -342,59 +259,33 @@ mod tests {
                 "{0:+3, 2:-4.1}/3",
                 (vec![0, 2], vec![F32(3.0), F32(-4.1)], 3),
             ),
-            ("{0:0, 1:0, 2:0}/3", (vec![], vec![], 3)),
+            (
+                "{0:0, 1:0, 2:0}/3",
+                (vec![0, 1, 2], vec![F32(0.0), F32(0.0), F32(0.0)], 3),
+            ),
             (
                 "{3:3, 2:2, 1:1, 0:0}/4",
-                (vec![1, 2, 3], vec![F32(1.0), F32(2.0), F32(3.0)], 4),
+                (
+                    vec![0, 1, 2, 3],
+                    vec![F32(0.0), F32(1.0), F32(2.0), F32(3.0)],
+                    4,
+                ),
             ),
-        ]);
-        for (e, ans) in exprs {
+        ];
+        for (e, parsed) in exprs {
             let ret = parse_pgvector_svector(e.as_bytes(), |s| s.parse::<F32>().ok());
             assert!(ret.is_ok(), "at expr {:?}: {:?}", e, ret);
-            assert_eq!(ret.unwrap(), ans, "at expr {:?}", e);
+            assert_eq!(ret.unwrap(), parsed, "parsed at expr {:?}", e);
         }
     }
 
     #[test]
     fn test_svector_parse_reject() {
-        let exprs: HashMap<&str, ParseVectorError> = HashMap::from([
+        let exprs: Vec<(&str, ParseVectorError)> = vec![
             ("{", ParseVectorError::BadParsing { position: 1 }),
             ("}", ParseVectorError::BadCharacter { position: 0 }),
             ("{:", ParseVectorError::BadCharacter { position: 1 }),
             (":}", ParseVectorError::BadCharacter { position: 0 }),
-            (
-                "{}/0",
-                ParseVectorError::InvalidDimension {
-                    dims: 0,
-                    min: 0,
-                    max: 1048576,
-                },
-            ),
-            (
-                "{}/1919810",
-                ParseVectorError::InvalidDimension {
-                    dims: 1919810,
-                    min: 0,
-                    max: 1048576,
-                },
-            ),
-            ("{0:1, 0:2}/1", ParseVectorError::IndexConflict { index: 0 }),
-            (
-                "{0:1, 1:1.5}/1",
-                ParseVectorError::OutOfBound { dims: 1, index: 1 },
-            ),
-            (
-                "{0:0, 1:0, 2:0}/2",
-                ParseVectorError::OutOfBound { dims: 2, index: 2 },
-            ),
-            (
-                "{2:0, 1:0}/2",
-                ParseVectorError::OutOfBound { dims: 2, index: 2 },
-            ),
-            (
-                "{2:0, 1:0, }/2",
-                ParseVectorError::OutOfBound { dims: 2, index: 2 },
-            ),
             (
                 "{0:1, 1:2, 2:3}",
                 ParseVectorError::BadParsing { position: 15 },
@@ -426,11 +317,53 @@ mod tests {
                 "{1,2,3,4}/5",
                 ParseVectorError::BadCharacter { position: 2 },
             ),
-        ]);
+        ];
         for (e, err) in exprs {
             let ret = parse_pgvector_svector(e.as_bytes(), |s| s.parse::<F32>().ok());
             assert!(ret.is_err(), "at expr {:?}: {:?}", e, ret);
-            assert_eq!(ret.unwrap_err(), err, "at expr {:?}", e);
+            assert_eq!(ret.unwrap_err(), err, "parsed at expr {:?}", e);
+        }
+    }
+
+    #[test]
+    fn test_svector_parse_filter() {
+        let exprs: Vec<(&str, (Vec<u32>, Vec<F32>, usize), (Vec<u32>, Vec<F32>))> = vec![
+            ("{}/0", (vec![], vec![], 0), (vec![], vec![])),
+            ("{}/1919810", (vec![], vec![], 1919810), (vec![], vec![])),
+            (
+                "{0:1, 0:2}/1",
+                (vec![0, 0], vec![F32(1.0), F32(2.0)], 1),
+                (vec![0, 0], vec![F32(1.0), F32(2.0)]),
+            ),
+            (
+                "{0:1, 1:1.5}/1",
+                (vec![0, 1], vec![F32(1.0), F32(1.5)], 1),
+                (vec![0, 1], vec![F32(1.0), F32(1.5)]),
+            ),
+            (
+                "{0:0, 1:0, 2:0}/2",
+                (vec![0, 1, 2], vec![F32(0.0), F32(0.0), F32(0.0)], 2),
+                (vec![], vec![]),
+            ),
+            (
+                "{2:0, 1:0}/2",
+                (vec![1, 2], vec![F32(0.0), F32(0.0)], 2),
+                (vec![], vec![]),
+            ),
+            (
+                "{2:0, 1:0, }/2",
+                (vec![1, 2], vec![F32(0.0), F32(0.0)], 2),
+                (vec![], vec![]),
+            ),
+        ];
+        for (e, parsed, filtered) in exprs {
+            let ret = parse_pgvector_svector(e.as_bytes(), |s| s.parse::<F32>().ok());
+            assert!(ret.is_ok(), "at expr {:?}: {:?}", e, ret);
+            assert_eq!(ret.unwrap(), parsed, "parsed at expr {:?}", e);
+
+            let (indexes, values, _) = parsed;
+            let nonzero = svector_filter_nonzero(&indexes, &values);
+            assert_eq!(nonzero, filtered, "filtered at expr {:?}", e);
         }
     }
 }
