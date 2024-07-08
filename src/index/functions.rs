f73bee9c61b3f1dd7c031fa606221544f8635dd9
usamoi <usamoi@outlook.com>
2024-07-08 14:13:23 +0800

    feat: add util function pg_vector_index_fence
    Signed-off-by: usamoi <usamoi@outlook.com>

--- src/index/functions.rs
+++ src/index/functions.rs
@@ -1,7 +1,41 @@
+use super::utils::from_oid_to_handle;
+use crate::error::check_client;
+use crate::ipc::client;
+use base::index::StatError;
+use pgrx::pg_sys::Oid;
+
 #[pgrx::pg_extern(volatile, strict, parallel_safe)]
 fn _vectors_pgvectors_upgrade() {
     if crate::bgworker::is_started() {
         return;
     }
     let _ = std::fs::remove_dir_all("pg_vectors");
+}
+
+#[pgrx::pg_extern(volatile, strict, parallel_safe)]
+fn _vectors_fence_vector_index(oid: Oid) {
+    let handle = from_oid_to_handle(oid);
+    let mut rpc = check_client(client());
+    loop {
+        pgrx::check_for_interrupts!();
+        match rpc.stat(handle) {
+            Ok(s) => {
+                if !s.indexing {
+                    break;
+                }
+            }
+            Err(StatError::NotExist) => pgrx::error!("internal error"),
+        }
+        unsafe {
+            pgrx::pg_sys::WaitLatch(
+                pgrx::pg_sys::MyLatch,
+                (pgrx::pg_sys::WL_LATCH_SET
+                    | pgrx::pg_sys::WL_TIMEOUT
+                    | pgrx::pg_sys::WL_EXIT_ON_PM_DEATH) as _,
+                1000,
+                pgrx::pg_sys::WaitEventTimeout_WAIT_EVENT_PG_SLEEP,
+            );
+            pgrx::pg_sys::ResetLatch(pgrx::pg_sys::MyLatch);
+        }
+    }
 }
