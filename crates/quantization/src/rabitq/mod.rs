1373b80a16ec37bce6bb126356dcdf0367fbc6fc
Keming <kemingyang@tensorchord.ai>
2024-07-19 12:43:47 +0800

    init rabitq
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- crates/quantization/src/rabitq/mod.rs
+++ crates/quantization/src/rabitq/mod.rs
@@ -1,10 +1,14 @@
+use std::ops::Div;
+
 use base::{
     index::{RaBitQuantizationOptions, SearchOptions, VectorOptions},
     operator::{Borrowed, Owned, Scalar},
     scalar::{ScalarLike, F32},
     search::{Reranker, Vectors},
+    vector::{VectorBorrowed, VectorOwned},
 };
 use nalgebra::{debug::RandomOrthogonal, Dim, Dyn};
+use num_traits::{Float, Zero};
 use operator::OperatorRaBitQ;
 use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
@@ -20,6 +24,9 @@ pub struct RaBitQuantizer<O: OperatorRaBitQ> {
     dim: u32,
     dim_pad_64: u32,
     projection: Vec<Vec<Scalar<O>>>,
+    binary_x: Vec<u64>,
+    distance_to_centroid: Vec<Scalar<O>>,
+    dot_product_x: Vec<Scalar<O>>,
 }
 
 impl<O: OperatorRaBitQ> RaBitQuantizer<O> {
@@ -31,11 +38,38 @@ impl<O: OperatorRaBitQ> RaBitQuantizer<O> {
     ) -> Self {
         let dim_pad = (vector_options.dims + 63) / 64 * 64;
         let projection = gen_random_orthogonal::<O>(dim_pad as usize);
+        let n = vectors.len();
+        let mut distance_to_centroid = vec![Scalar::<O>::zero(); n as usize];
+        let mut quantized_x = vec![vec![Scalar::<O>::zero(); dim_pad as usize]; n as usize];
+        for i in 0..n {
+            let vector = transform(vectors.vector(i)).as_borrowed().to_vec();
+            distance_to_centroid[i as usize] = O::vector_dot_product(&vector, &vector).sqrt();
+            for j in 0..vector_options.dims as usize {
+                quantized_x[i as usize][j] = O::vector_dot_product(&projection[j], &vector);
+            }
+        }
+        let mut binary_vec_x = Vec::with_capacity(n as usize);
+        let mut signed_x = Vec::with_capacity(n as usize);
+        for i in 0..(n as usize) {
+            binary_vec_x.push(O::vector_binarize_u64(&quantized_x[i]));
+            signed_x.push(O::vector_binarize_one(&quantized_x[i]));
+        }
+        let binary_x: Vec<u64> = binary_vec_x.into_iter().flatten().collect();
+        let mut dot_product_x = vec![Scalar::<O>::zero(); n as usize];
+        for i in 0..(n as usize) {
+            let norm = O::vector_dot_product(&quantized_x[i], &quantized_x[i]).sqrt()
+                * Scalar::<O>::from_f32(dim_pad as f32).sqrt();
+            dot_product_x[i as usize] =
+                O::vector_dot_product(&quantized_x[i], &signed_x[i]).div(norm);
+        }
 
         Self {
             dim: vector_options.dims,
             dim_pad_64: dim_pad,
             projection,
+            binary_x,
+            distance_to_centroid,
+            dot_product_x,
         }
     }
 
@@ -103,6 +137,7 @@ fn gen_random_orthogonal<O: OperatorRaBitQ>(dim: usize) -> Vec<Vec<Scalar<O>>> {
     let random_matrix = random_orth.unwrap();
     let mut projection = vec![Vec::with_capacity(dim); dim];
     // use the transpose of the random matrix as the inverse of the projection matrix
+    // TODO: inverse or not?
     for (i, vec) in random_matrix.column_iter().enumerate() {
         for val in vec.iter() {
             projection[i].push(ScalarLike::from_f32(*val));
--- crates/quantization/src/rabitq/operator.rs
+++ crates/quantization/src/rabitq/operator.rs
@@ -1,9 +1,42 @@
-use base::operator::{Operator, Owned};
+use base::operator::{Operator, Owned, Scalar};
+use num_traits::{Float, One, Zero};
 
 pub trait OperatorRaBitQ: Operator {
     type RabitQuantizationPreprocessed;
+
+    fn vector_dot_product(lhs: &[Scalar<Self>], rhs: &[Scalar<Self>]) -> Scalar<Self>;
+    fn vector_binarize_u64(vec: &[Scalar<Self>]) -> Vec<u64>;
+    fn vector_binarize_one(vec: &[Scalar<Self>]) -> Vec<Scalar<Self>>;
 }
 
 impl<O: Operator> OperatorRaBitQ for O {
     type RabitQuantizationPreprocessed = Owned<O>;
+
+    fn vector_dot_product(lhs: &[Scalar<O>], rhs: &[Scalar<O>]) -> Scalar<O> {
+        let mut sum = Scalar::<O>::zero();
+        for i in 0..std::cmp::min(lhs.len(), rhs.len()) {
+            sum += lhs[i] * rhs[i];
+        }
+        sum
+    }
+
+    fn vector_binarize_u64(vec: &[Scalar<Self>]) -> Vec<u64> {
+        let mut binary = vec![0u64, (vec.len() as u64 + 63) / 64];
+        for i in 0..vec.len() {
+            if vec[i].is_sign_positive() {
+                binary[i / 64] |= 1 << (i % 64);
+            }
+        }
+        binary
+    }
+
+    fn vector_binarize_one(vec: &[Scalar<Self>]) -> Vec<Scalar<Self>> {
+        let mut binary = vec![Scalar::<O>::one(); vec.len()];
+        for i in 0..vec.len() {
+            if vec[i].is_sign_negative() {
+                binary[i] = -Scalar::<O>::one();
+            }
+        }
+        binary
+    }
 }
