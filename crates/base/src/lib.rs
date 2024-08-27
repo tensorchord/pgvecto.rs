eb87a9d5cb7c1a41e1e964a0f35bafe677085385
usamoi <usamoi@outlook.com>
2024-08-27 20:42:35 +0800

    feat: mark Distance::from_f32 and Distance::to_f32 as const
    Signed-off-by: usamoi <usamoi@outlook.com>

--- crates/base/src/lib.rs
+++ crates/base/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(const_float_bits_conv)]
 #![feature(avx512_target_feature)]
 #![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx512))]
 #![cfg_attr(target_arch = "x86_64", feature(stdarch_x86_avx512_f16))]
